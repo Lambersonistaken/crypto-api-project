d151b04ee72f6db8f5607ae04d36ccf0a7d10546
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2024-10-23 02:13:20 +0300

    context created

---
+++ src/context/CoinContext.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+import { createContext } from 'react';
+
+export const CoinContext = createContext();
+
+const CoinContextProvider = (props) => {
+
+
+  const contextValue = {};
+
+
+  return (
+    <CoinContext.Provider value={{contextValue}}>
+      {props.children}
+    </CoinContext.Provider>
+  );
+}
+
+
+export default CoinContextProvider;
